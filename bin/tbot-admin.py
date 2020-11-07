1565e96fffd6c16035fe4f006e0e164fa1c42b90
Gaspect <jesusefg12@gmail.com>
2020-11-07 13:11:59 +0100

    tbot_admin.py updated
    Finally created main command and som minnor updates tp setup.py

--- bin/tbot-admin.py
+++ bin/tbot-admin.py
@@ -1,5 +1,42 @@
 import os
 
+files_and_content = [('settings.py', """import os
+
+BASEDIR = os.path.dirname(__file__)
+
+SECTIONS = []
+GLOBALS = []
+
+DB = {
+    'engine': 'sqlite',
+    'name': '/sqlite.db',
+}
+
+TOKEN = ''
+PROXY = {}
+"""),
+                     ('botmanager.py',"""import os
+from bot_framework.shell.loader import handle_shell_input
+
+import sys
+
+
+def main():
+    os.environ.setdefault('BOT_SETTING_MODULE', 'settings')
+    handle_shell_input(*sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
+""")]
+
 
 def create_project(project_name):
-    os.mkdir()
+    full_pn = os.path.join(os.getcwd(), project_name)
+    if os.path.exists(full_pn):
+        return 'A folder with that name already exist'
+    else:
+        os.mkdir(full_pn)
+        for fal in files_and_content:
+            with open(os.path.join(full_pn,fal[0]),'w') as fs:
+                fs.write(fal[1])
--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+description = ''
+with open('README.md', 'r') as f:
+    description = f.read()
+
 setup(
     name='easy_tbot',
     version='1.0',
     packages=['easy_tbot', 'easy_tbot.db', 'easy_tbot.bot', 'easy_tbot.shell', 'easy_tbot.handlers',
               'easy_tbot.handlers.setup'],
     url='',
-    license='GNU LGPLv3',
+    license='GNU LESSER GENERAL PUBLIC LICENSE',
     author='Jesús Enrique Fuentes González',
     author_email='jesusefg12@gmail.com',
-    description='Framework  for data base and other usefull stuff integration with Telegram bot api'
+    description='Framework  for data base and other usefull stuff integration with Telegram bot api',
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
