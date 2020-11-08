4efd3b40cb9ee206173a7db9c12a437d75a2b6a7
Gaspect <jesusefg12@gmail.com>
2020-11-08 16:11:53 +0100

    Update middleware.py
    Added all updates types in middleware

--- easy_tbot/handlers/middleware.py
+++ easy_tbot/handlers/middleware.py
@@ -8,14 +8,23 @@ import typing
 
 
 class UpdateType(Enum):
-    pass  # TODO: update this
+    MESSAGE = 'message'
+    EDITED_MESSAGE = 'edited_message'
+    CHANEL_POST = 'channel_post'
+    EDITED_CHANEL_POST = 'edited_channel_post'
+    INLINE_QUERY = 'inline_query'
+    CHOSEN_INLINE_RESULT = 'chosen_inline_result'
+    CALLBACK_QUERY = 'callback_query'
+    SHIPPING_QUERY = 'shipping_query'
+    PRE_CHECKOUT_QUERY = 'pre_checkout_query'
+    POLL = 'poll'
 
 
 class Middleware(HandlerSetup, ABC):
     """
     Base class for a middleware. This manage and add needed info
     """
-    update_types: typing.List[UpdateType]
+    update_types: typing.List[UpdateType] = [UpdateType.MESSAGE]
 
     @abstractmethod
     def middleware(self, bot: TeleBot, msg: Message):
