8a48648144d434a7f52352b4d660c6187d4227d8
Gaspect <jesusefg12@gmail.com>
2020-11-13 16:44:46 -0500

    Update __init__.py

--- easy_tbot/__init__.py
+++ easy_tbot/__init__.py
@@ -1,9 +1,10 @@
 from .bot.bot import Bot
-from telebot.types import *
+from telebot import types
 from .db.models import Model, Session, session_scope
 from .handlers.handler import Command, Regex, Function, All
 from .handlers.mixing import Mixing
 from .handlers.middleware import Middleware
+from .handlers.inline import InlineHandler
 from .shell.shell import ShellCommand
 from .utils import method_decorator as detach
 from .render.text import render
