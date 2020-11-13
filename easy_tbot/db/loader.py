518f82702c92cd0a1b3d53b41f1898d456eb8622
Gaspect <jesusefg12@gmail.com>
2020-11-13 17:57:58 -0500

    Update loader.py

--- easy_tbot/db/loader.py
+++ easy_tbot/db/loader.py
@@ -8,7 +8,7 @@ import inspect
 
 
 @Cached
-def load_dbengine():
+def load_db_engine():
     """
     Gets the database engine for the project.
     :return: Base (db engine class of sqlalchemy package).
@@ -32,25 +32,25 @@ def get_session_class():
     Constructs a new class type for defining sessions in the data base engine.
     :return: New class for definig sessions.
     """
-    return sessionmaker(load_dbengine())
+    return sessionmaker(load_db_engine())
 
 
 def migrate():
     """
     Write the proper tables in the database, sometimes it generates a database (sqlite).
-    :return: All subcriptions models of the database, the datatables in raw format.
+    :return: All subscriptions models of the database, the datatables in raw format.
     """
-    subcriptions = []
+    subscriptions = []
     model = get_model()
 
     def handle_app(app):
         get_logger().info(f'Analysing {app}')
         app_module = import_module(f'{app}.models')
         for table in inspect.getmembers(app_module, inspect.isclass):
-            if issubclass(table[1], model) and table[1] is not model and table[1].__table__ not in subcriptions:
-                subcriptions.append(table[1].__table__)
+            if issubclass(table[1], model) and table[1] is not model and table[1].__table__ not in subscriptions:
+                subscriptions.append(table[1].__table__)
                 get_logger().info(f'Successfully analyzed {table[0]}')
 
     for_app_do(handle_app)
-    model.metadata.create_all(load_dbengine(), tables=subcriptions)
-    return subcriptions
+    model.metadata.create_all(load_db_engine(), tables=subscriptions)
+    return subscriptions
