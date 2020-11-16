36a0afc453166c23e2f37d84bf1d45db020e4208
Gaspect <jesusefg12@gmail.com>
2020-11-15 21:08:55 -0500

    Update mixing.py

--- easy_tbot/handlers/mixing.py
+++ easy_tbot/handlers/mixing.py
@@ -2,10 +2,24 @@ from .handler import HandlerSetup
 import inspect
 
 
+class MixingException(Exception):
+    pass
+
+
 class Mixing(HandlerSetup):
     """
     This class must be first inheritance in a handler class mixing
     """
+    def __init__(self, bot):
+        super().__init__(bot)
+        mro = self.__class__.__mro__
+        if self.__class__ == Mixing:
+            raise MixingException("This class can't be instantiated directly")
+        elif len(mro) < 3:
+            raise MixingException('Mixing class must be follow by classes to mix')
+        elif mro[1] != Mixing:
+            raise MixingException('Mixing class must be first in inheritance declaration')
+
     def setup(self):
         for cls in self.__class__.__mro__[2:]:
             if hasattr(cls, 'setup') and not inspect.isabstract(cls):
